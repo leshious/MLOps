d23370a5f7a9db8ef97d6c600a56e7f65f826abe
leksious <72123355+leksious@users.noreply.github.com>
2023-10-07 05:12:15 +0300

    added train and infer

---
+++ infer.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pickle
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, f1_score
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def main():
+    iris = datasets.load_iris()
+    X = iris.data
+    y = iris.target
+
+    # Split the dataset
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, stratify=y, random_state=42)
+
+    with open('logistic_regression_model.pkl', 'rb') as file:
+        loaded_model = pickle.load(file)
+
+    y_test_pred = loaded_model.predict(X_test)
+
+    test_accuracy = accuracy_score(y_test, y_test_pred)
+    print(test_accuracy)
+    print(precision_score(y_test, y_test_pred, average='macro'))  # added average='macro' for multi-class case
+    print(f1_score(y_test, y_test_pred, average='macro'))         # added average='macro' for multi-class case
+
+    results_df = pd.DataFrame({
+        'Predictions': y_test_pred
+    })
+
+    # Save the dataframe to a .csv file
+    results_df.to_csv('results.csv', index=False)
+
+if __name__ == '__main__':
+    main()
---
+++ train.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import pickle
+from sklearn.model_selection import train_test_split
+
+
+def main():
+    # Load the Iris dataset
+    iris = datasets.load_iris()
+    X = iris.data
+    y = iris.target
+
+    # Split the dataset
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, stratify=y, random_state=42)
+
+    # Initialize and train the logistic regression model
+    clf = LogisticRegression(max_iter=1000).fit(X_train, y_train)
+
+    with open('logistic_regression_model.pkl', 'wb') as file:
+        pickle.dump(clf, file)
+
+if __name__ == '__main__':
+    main()
