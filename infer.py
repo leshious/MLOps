86044a7f365fe45dd787e179e359a698749bf3fa
leksious <72123355+leksious@users.noreply.github.com>
2023-12-03 20:04:28 +0300

    some refactorings

--- infer.py
+++ infer.py
@@ -1,35 +1,31 @@
 import pickle
 
-import pandas as pd
-from sklearn import datasets
-from sklearn.metrics import accuracy_score, f1_score, precision_score
-from sklearn.model_selection import train_test_split
 import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score, f1_score, precision_score
 
 
 def main():
-    X_train = np.load('..data/X_train.npy')
-    X_test = np.load('..data/X_test.npy')
-    y_train = np.load('..data/y_train.npy')
-    y_test = np.load('..data/y_test.npy')
+    X_train = np.load("data/X_train.npy")
+    X_test = np.load("data/X_test.npy")
+    y_train = np.load("data/y_train.npy")
+    y_test = np.load("data/y_test.npy")
 
     with open("logistic_regression_model.pkl", "rb") as file:
         loaded_model = pickle.load(file)
 
     y_test_pred = loaded_model.predict(X_test)
+    y_train_pred = loaded_model.predict(X_train)
 
     test_accuracy = accuracy_score(y_test, y_test_pred)
+    train_accuracy = accuracy_score(y_train, y_train_pred)
     print(test_accuracy)
-    print(
-        precision_score(y_test, y_test_pred, average="macro")
-    )  # added average='macro' for multi-class case
-    print(
-        f1_score(y_test, y_test_pred, average="macro")
-    )  # added average='macro' for multi-class case
+    print(precision_score(y_test, y_test_pred, average="macro"))
+    print(f1_score(y_test, y_test_pred, average="macro"))
+    print(train_accuracy)
 
     results_df = pd.DataFrame({"Predictions": y_test_pred})
 
-    # Save the dataframe to a .csv file
     results_df.to_csv("results.csv", index=False)
 
 
--- train.py
+++ train.py
@@ -1,13 +1,11 @@
 import pickle
 
-from sklearn import datasets
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-import numpy as np
 import hydra
-import os
+import numpy as np
+from sklearn.linear_model import LogisticRegression
 
-@hydra.main(config_path="configs", config_name="logistic_regression_config", version_base="1.2")
+
+@hydra.main(config_path="configs", config_name="lr_config", version_base="1.2")
 def main(cfg):
     working_dir = hydra.utils.get_original_cwd()
 
@@ -15,11 +13,10 @@ def main(cfg):
     data_path = f"{working_dir}/{cfg.data.data_dir}/"
 
     # Загружаем данные
-    X_train = np.load(data_path + 'X_train.npy')
-    X_test = np.load(data_path + 'X_test.npy')
-    y_train = np.load(data_path + 'y_train.npy')
-    y_test = np.load(data_path + 'y_test.npy')
-
+    X_train = np.load(data_path + "X_train.npy")
+    # X_test = np.load(data_path + "X_test.npy")
+    y_train = np.load(data_path + "y_train.npy")
+    # y_test = np.load(data_path + "y_test.npy")
 
     # Initialize and train the logistic regression model
     clf = LogisticRegression(
